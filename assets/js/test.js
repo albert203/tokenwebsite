b14f47503dfb5d2cf8b3bf9e0346feccfba02eec
albert203 <47762213+albert203@users.noreply.github.com>
2022-06-26 03:07:53 +1200

    background and some animations added

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,51 +1,14 @@
-//create a moving object
-var movingObject = {
-    x: 0,
-    y: 0,
-    speed: 1,
-    direction: "right",
-    move: function () {
-        if (this.direction === "right") {
-            this.x += this.speed;
-        } else if (this.direction === "left") {
-            this.x -= this.speed;
-        } else if (this.direction === "up") {
-            this.y -= this.speed;
-        } else if (this.direction === "down") {
-            this.y += this.speed;
-        }
-    }
-}
-//create a second moving object
-var movingObject2 = {
-    x: 0,
-    y: 0,
-    speed: 1,
-    direction: "right",
-    move: function () {
-        if (this.direction === "right") {
-            this.x += this.speed;
-        } else if (this.direction === "left") {
-            this.x -= this.speed;
-        } else if (this.direction === "up") {
-            this.y -= this.speed;
-        } else if (this.direction === "down") {
-            this.y += this.speed;
-        }
-    }
-}
+let moon = document.querySelector('#moon');
+let mountains = document.querySelector('#mountains');
+let stars = document.querySelector('#stars');
+let landscape = document.querySelector('#landscape');
+let text = document.querySelector('#text');
+let button = document.querySelector('.button');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains.style.top = value * 1.05 + 'px';
+    landscape.style.top = value * 0 + 'px';
+});
